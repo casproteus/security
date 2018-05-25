75c1888637e4c77e6253dc66b058cedf7426bb07
tao <casproteus@hotmail.com>
2018-05-25 00:05:22 -0400

    same as last commit.

---
+++ simonstyle/src/main/java/org/cas/client/platform/bar/i18n/BarDlgConst.java
@@ -0,0 +1,232 @@
+package org.cas.client.platform.bar.i18n;
+
+public interface BarDlgConst {
+    int SCROLLBAR_WIDTH = 40;
+    int SubTotal_HEIGHT = 50;
+
+    String OK();
+    String Cancel();
+    String ChangeDate();
+    String ViewDetail();
+    String Close();
+    String Tip();
+    String ServiceFee();
+    String CashBack();
+    String Status();
+    String APPLY();
+
+    String EnterCashPayment();
+    String EnterDebitPayment();
+    String EnterVisaPayment();
+    String EnterMasterPayment();
+    
+    String ChangeMode();
+    String AddTable();
+    String OrderManage();
+    String OpenDrawer();
+    String VolumnDiscount();
+    String WaiterReport();
+    String Reservation();
+    String Report();
+    String CheckInOut();
+    
+    String Menu();
+    String Language1();
+    String Language2();
+    String Language3();
+    String DSPINDEX();
+    String PRICE();
+
+    String EXACT_AMOUNT();
+    String CASH();
+    String PAY();
+    String REMOVEITEM();
+    String VOID_ITEM();
+    String SPLIT_BILL();
+    String Modify();
+    String ChangePrice();
+    String TIP();
+    String QTY();
+    String QTYNOTICE();
+    String DISC_ITEM();
+    String PRINT_BILL();
+
+    String DEBIT();
+    String VISA();
+    String MASTER();
+    String CANCEL_ALL();
+    String VOID_ORDER();
+    String SETTINGS();
+    String RETURN();
+    String MORE();
+    String SEND();
+
+    String EQUL_BILL();
+    String FAST_DISCOUNT();
+    String QUICK_OPEN();
+    String MODIFY();
+    String DISC_VOLUMN();
+
+    String PRINTER();
+    String OnlyOneShouldBeSelected();
+    String InvalidInput();
+    String ADMIN_MODE();
+    String USE_MODE();
+    String Title();
+    String DuplicatedInput();
+    String NoBillSeleted();
+    String NotPayYet();
+    String AllreadyRefund();
+    String UnSendRecordFound();
+    String Colon();
+    String Operator();
+    String TABLE();
+    String Bill();
+    String UnSavedRecordFound();
+    String COMFIRMDELETEACTION();
+    String COMFIRMDELETEACTION2();
+    String COMFIRMLOSTACTION();
+    String ConfirmPayAgain();
+    String SetCatogoryFirst();
+    String SendItemCanNotModify();
+    String reCeivedMoneyNotEnough();
+    String LeftMoney();
+    String UnSavedContentWillBeLost();
+    String Left();
+    String OPENTIME();
+    String EndTime();
+
+    String EXACT();
+
+    String BillPageRow();
+    String BillPageCol();
+    String IsSingleUser();
+    String IsDiscBeforeTax();
+    String IsPrintBillWhenPay();
+    String StartTimeOfDay();
+    String PrinterError();
+    
+    String Other();
+    String ProdNumber();
+    String ProdName();
+    String Count();
+    String Price();
+    String Subtotal();
+    String Total();
+    String QST();
+    String GST();
+    String PricePomp();
+    String MenuPomp();
+    String ModifyPomp();
+
+
+    String Discount();
+    String Receive();
+    String comment();
+    String OpenTime();
+    
+    String Change();
+
+    String AddUser();
+    String PrintAll();
+
+    String EqualBill();
+    String SplitItem();
+    String MoveItem();
+    String CombineAll();
+    String Refund();
+    String CompleteAll();
+    String CancelAll();
+    String ProdCodeLength();
+    String Shoestring();
+    String Size();
+    String[] Sizes();
+    String Name();
+    String IPAddress();
+    String Categary();
+    String TIME();
+    
+    String PrinterSupport();
+    
+    String SaleRecs();
+    String FROM();
+    String TO();
+
+    String EMPLOYEEID();
+    String PHOTO();
+    String LASTNAME();
+    String FIRSTNAME();
+    String DISPLAYAS();
+    String NICKNAME();
+    String JOB();// job
+    String RELATION();
+    String IS_LADY();
+    String PASSWORD();
+    String ANNIVERSARY1();
+    String ANNIVERSARY2();
+    String JOIN_TIME();
+    String ADDRESS();
+    String PHONE();
+    String MOBILE();
+    String EMAIL();
+    String lang1();
+    String lang2();
+    String lang3();
+    String lang4();
+    String SSCN();
+    String SALARY();
+    String WEBPAGE();
+    String[] langs();
+    String[] ANNIVERSARYTYPE();;
+    
+    String GENERAL();
+    String EmployeeInfo();
+    String JobTitle();
+    String NickName();
+    String Cellphone();
+    String PhoneNum();
+    String HomeAddress();
+    String MailAddress();
+    String Account();
+
+    String CompName();
+    String CompAddress();
+    String CompTel();
+    String CompFax();
+
+    String QQ();
+    String MainPage();
+    String Sex();
+    String JoinTime();
+    String Salary();
+    String INSURANCE();
+    String SSCNUMBER();
+    String IDCARD();
+    String BIRTHDAY();
+    String BANKNUMBER();
+    String Type();
+    String Note();
+    
+    String Add();
+
+    String WorkRecs();   
+    String MoneyInBox();
+    String Calculate();
+    String Profit();
+    
+    String Product();
+    String InputRecs();
+    String Supplier();
+    
+    String PasswordMakeSure();
+    String PasswordNotEqual();
+    
+    String MUser();
+    String[] USERTYPE();
+
+    String AddNewUser();
+    String ModiFyUser();
+    String DeleteUser();
+    
+    String UserNameInUsed();
+}
