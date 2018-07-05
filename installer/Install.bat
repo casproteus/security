fbe57d001f3c173a054d5b01b32c0a1fb93579ea
Tao Jiang <taoj@opentext.com>
2018-07-05 12:38:22 -0400

    added dll into jre.

--- installer/Install.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-if "%OS%"=="Windows_NT" goto nt
-echo This script only works with NT-based versions of Windows.
-goto :eof
-
-:nt
-"%~dp0\jre\bin\java" -jar stgo-security-service-installer-1-SNAPSHOT-standard.jar
