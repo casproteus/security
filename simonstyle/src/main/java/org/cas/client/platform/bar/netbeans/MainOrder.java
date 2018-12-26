50d16445aea0cfa4302886208a0001c40ad8a015
tao <casproteus@hotmail.com>
2018-12-26 00:53:21 -0500

    Bug fix: modified client to Person type in MainOrder, so the json string can be unserialized.

--- simonstyle/src/main/java/org/cas/client/platform/bar/netbeans/MainOrder.java
+++ simonstyle/src/main/java/org/cas/client/platform/bar/netbeans/MainOrder.java
@@ -1,25 +1,28 @@
 package org.cas.client.platform.bar.netbeans;
 
+import java.util.Date;
+
 public class MainOrder {
 	//"delieverdate":1545066161000,
 	public long id;
-	public int version;
-    public String client;
+    public Person client;
     public String clientSideOrderNumber;
     public String clientSideModelNumber;
+    public String colorCard;
+    public Boolean colorProportionReceived;
+    public Person contactPerson;
+    public Date delieverdate;
     public String model;
+    public String payCondition;
     public String picture;
     public String productName;
-    public Person contactPerson;
     public int quantity;
-    public String payCondition;
-    public Boolean colorProportionReceived;
-    public long delieverdate;
-    public String sizeTable;
-    public String colorCard;
-    public String testRequirement;
-    public String sampleRequirement;
-    public String remark;
-    public Person person;
     public int recordStatus;
+    public String remark;
+    public String sampleRequirement;
+    public String sizeTable;
+    public String testRequirement;
+    public Person person;
+	public int version;
+    
 }
