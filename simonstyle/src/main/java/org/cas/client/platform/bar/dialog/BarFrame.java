51a0dd69b859e80cef456d2565b900eb237f9a69
tao <casproteus@hotmail.com>
2019-03-29 13:47:30 -0400

    version update

--- simonstyle/src/main/java/org/cas/client/platform/bar/dialog/BarFrame.java
+++ simonstyle/src/main/java/org/cas/client/platform/bar/dialog/BarFrame.java
@@ -45,7 +45,7 @@ import org.cas.client.platform.pimmodel.PIMRecord;
 import org.json.JSONObject;
 
 public class BarFrame extends JFrame implements ICASDialog, WindowListener, ComponentListener, ItemListener {
-	private String VERSION = "V0.188-20190326";
+	private String VERSION = "V0.189-20190329";
 	public static BarFrame instance;
     public static BarDlgConst consts = new BarDlgConst0();
     
